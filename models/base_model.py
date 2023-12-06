e59bc51917a3a5bf343adb94951a8751dcf2a493
NwoguGlory1 <glorynwogu18@gmail.com>
2023-12-06 00:57:10 -0800

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,9 @@ import uuid
 
 class BaseModel:
     """A BaseModel class that defines all common attributes/methods for other classes"""
+
     def __init__(self, *args, **kwargs):
+        """Constructor"""
         self.id = str(uuid.uuid4())
         """Assign with an uuid when an instance is created """
         self.created_at = datetime.now()
@@ -20,13 +22,17 @@ class BaseModel:
                     setattr(self, key, value)
                 else:
                     setattr(self, key, value)
+
+
     def __str__(self):
         """Provides a string representation of an object & displays it"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
